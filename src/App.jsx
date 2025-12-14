2b35c28e922aa85773392ff3c25346aea279f972
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-14 09:36:38 +0530

    feat: add About section to App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
 
 const App = () => {
     return (
         <div className='container mx-auto max-w-7xl'>
             <Navbar />
             <Hero />
+            <About />
+            <section className='min-h-screen flex items-center justify-center' />
             {/* about */}
             {/* projects */}
             {/* experience */}
